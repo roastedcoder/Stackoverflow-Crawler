ada0218ed09affa5b82cb8f1f01359fde47b62c9
Suraj Kumar <vermasuraj412@gmail.com>
2022-02-01 23:14:04 +0530

    maintained a concurrency of 5 requests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # stackoverflow crawler
 
 * used **_cheerio_** library for **_node.js_**
+* tried maintaining concurrency of 5 users using cluster.
+* data scraped will be saved in a .csv file
--- app.js
+++ app.js
@@ -5,29 +5,53 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const express = require('express');
-const writeStream = fs.createWriteStream('data.csv');
+const http = require('http');
+const cluster = require('cluster');
+const cpus = require('os').cpus();
 
-writeStream.write(`QuestionURL, Views, Upvotes, Answers \n`);
 
 const app = express();
+
+// creating a writestream using filesystem library
+const writeStream = fs.createWriteStream('data.csv');
+writeStream.write(`QuestionURL, Views, Upvotes, Answers, pID \n`);
+
+
 const BASE_URL = 'https://stackoverflow.com/';
+const pages = 100;
 
-
-request('https://stackoverflow.com/questions?page=1&pagesize=50', function (err, res, body) {
-    if(err) console.log(err, "error occured while hitting URL");
-    else {
-        const $ = cheerio.load(body);
-
-        $('.question-summary').each(function(i, e) {
-            const href = $(e).find('.question-hyperlink').attr('href');
-            const views = parseInt($(e).find('.views').html());
-            const upvotes = $(e).find('.vote-count-post').find('strong').html();
-            const answers = $(e).find('.status').find('strong').html();
-
-            const questionURL = BASE_URL + href;
-
-            writeStream.write(`${questionURL}, ${views}, ${upvotes}, ${answers} \n`);
-        });
-        console.log('Crawling Done...');
+if(cluster.isMaster) {
+    console.log(`Master ${process.pid} is running...`);
+    for(let i = 0; i<5; i++) { // scaling upto 5 concurrent users
+        cluster.fork();
     }
-});
+}
+else { // using worker processes to achieve concurrency
+    const message = `Process ${process.pid} started...`
+    console.log(message);
+    scrapData(process.pid); // scraping and saving the data into .csv
+}
+function scrapData(pID) { // scraping the data using cheerio
+    for(let i = 1; i<=pages; i++) {
+        request(`https://stackoverflow.com/questions?tab=newest&page=${i}`, function (err, res, body) {
+            if(err) console.log(err, "error occured while hitting URL");
+            else {
+                const $ = cheerio.load(body); // getting the DOM of the required web-page
+
+                
+                $('.question-summary').each(function(i, e) {
+                    const href = $(e).find('.question-hyperlink').attr('href');
+                    const views = parseInt($(e).find('.views').html());
+                    const upvotes = $(e).find('.vote-count-post').find('strong').html();
+                    const answers = $(e).find('.status').find('strong').html();
+    
+                    const questionURL = BASE_URL + href;
+    
+                    writeStream.write(`${questionURL}, ${views}, ${upvotes}, ${answers}, ${pID} \n`);
+                });
+                
+                console.log('Crawling Done...');
+            }
+        });
+    }
+}
